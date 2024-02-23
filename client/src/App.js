f5ac2faf5e8ad6dd953f0f2bf9729b01dadd9c89
Preethi84 <preethi18.c@gmail.com>
2024-02-23 21:05:51 +0530

    gitignore

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+import {Route, Routes} from "react-router-dom";
+
+import Navbar from "./components/navbar";
+
+const App = () => {
+    return(
+        <div>
+            <Navbar/>
+        </div>
+    );
+};
+
+export default App;
